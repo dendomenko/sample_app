cb5d2c29882612cf9a5b01a8601d6b2b38293852
dendomenko <dendomenko@gmail.com>
2016-09-25 17:16:58 +0300

    finish add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
+  match '/register', to: 'user#new',             via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
