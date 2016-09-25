8ffb2b1b0e9210ab43cf3f528e0fa8593c3fb563
dendomenko <dendomenko@gmail.com>
2016-09-25 20:18:06 +0300

    Finish sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  resources :users
 
   root 'static_pages#home'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/register', to: 'user#new',             via: 'get'
+  match '/register', to: 'users#new',             via: 'get'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
