d160f1e84698ffbdb04f511702ba7975c04967e0
dendomenko <dendomenko@gmail.com>
2016-09-25 16:52:46 +0300

    Add some style and user controller

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'user/new'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
