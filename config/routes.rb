98868cc4529c0bde29d0637a644eb0bd2260265c
dendomenko <dendomenko@gmail.com>
2016-09-25 15:54:53 +0300

    Added some changes to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
