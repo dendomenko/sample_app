f9e3ee36f376c68b493173d32c58de0bf3554d87
Arthur <nesterenko19@gmail.com>
2017-04-23 20:51:07 +0300

    make ajax request

--- client/app/js/containers/App/index.jsx
+++ client/app/js/containers/App/index.jsx
@@ -1,88 +1,35 @@
 import React from 'react';
-import axios from 'axios';
+import api from './../../utils/Api';
 
-
-axios.defaults = {
-    baseURL: '/api/v1/',
-    proxy  : {
-        host: '127.0.0.1',
-        port: 3000
-    }
-};
-
-
-function xdr( url, method, data, callback, errback ) {
-    var req;
-
-    if ( XMLHttpRequest ) {
-        req = new XMLHttpRequest();
-
-        if ( 'withCredentials' in req ) {
-            req.open( method, url, true );
-            req.onerror            = errback;
-            req.onreadystatechange = function () {
-                if ( req.readyState === 4 ) {
-                    if ( req.status >= 200 && req.status < 500 ) {
-                        callback( req.responseText );
-                    } else {
-                        errback( new Error( 'Response returned with non-OK status' ) );
-                    }
-                }
-            };
-            console.dir( data );
-            req.send( data );
-        }
-    } else if ( XDomainRequest ) {
-        req = new XDomainRequest();
-        req.open( method, url );
-        req.onerror = errback;
-        req.onload  = function () {
-            callback( req.responseText );
-        };
-        console.dir( data );
-        req.send( data );
-    } else {
-        errback( new Error( 'CORS not supported' ) );
-    }
-}
+// axios.defaults.proxy = {     host: '127.0.0.1',     port: 3000 };
+// axios.defaults.baseURL = 'localhost:3000/api/v1/';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
-
     componentWillMount() {
-        axios.defaults = {
-            baseURL: '/api/v1/',
-            proxy  : {
-                host: '127.0.0.1',
-                port: 3000
+        // axios.defaults = {     baseURL: '/api/v1/',     proxy: {         host:
+        // '127.0.0.1',         port: 3000     } }; const user = {     "user": { "name"
+        // : "arthur", "email"                : "arthur@mail.com", "password" :
+        // "12345678", "password_confirmation": "12345678"     } };
+        api
+            .post('/users', {
+            "user": {
+                "name": "denis",
+                "email": "denis@mail.com",
+                "password": "12345678",
+                "password_confirmation": "12345678"
             }
-        };
-
-        // const user = {
-        //     "user": {
-        //         "name"                 : "arthur",
-        //         "email"                : "arthur@mail.com",
-        //         "password"             : "12345678",
-        //         "password_confirmation": "12345678"
-        //     }
-        // };
-
-        xdr( 'http://127.0.0.1:3000/api/v1/users/', 'post', {
-                "user": {
-                    "name"                 : "arthur",
-                    "email"                : "arthur@mail.com",
-                    "password"             : "12345678",
-                    "password_confirmation": "12345678"
-                }
-            }, function ( data ) {
-                console.log( data );
-            },
-            function ( err ) {
-                console.log( err );
-            } );
+        })
+            .then(res => console.log(res))
+            .catch(err => console.info(err));
+        // xdr( 'http://127.0.0.1:3000/api/v1/users/', 'post', {         "user": {
+        // "name"                 : "arthur",             "email"                :
+        // "arthur@mail.com",             "password"             : "12345678",
+        // "password_confirmation": "12345678"         }     }, function ( data ) {
+        // console.log( data );     },     function ( err ) {         console.log( err
+        // );     } );
 
     }
-
 
     render() {
         return (
---
+++ client/app/js/utils/Api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api/v1/',
+    proxy: {
+        host: '127.0.0.1',
+        port: 3000
+    }
+});
+
+export default api;
