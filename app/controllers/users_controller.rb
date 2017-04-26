a80b3130218be47fc4aae44a7f13d2fa8ea92497
dendomenko <dendomenko@gmail.com>
2017-04-26 13:08:04 +0300

    Return base controller and debug into console

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,27 @@
-
 class UsersController < ApplicationController
-
-
   def new
     @user = User.new
   end
 
-  def index
-    users = User.all
-    render json: users, :status => '200'
-  end
-
   def show
-    user = User.find(params[:id])
-    render json: user, :status => '200'
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
-    @user.save!
-    # if @user.save
-    #   sign_in @user
-    #   flash[:success] = "Thank You for join to AntHill Task Manager"
-    #   redirect_to @user
-    # else
-    #   render 'new'
-    # end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Thank You for join to AntHill Task Manager"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-   private
+  private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
