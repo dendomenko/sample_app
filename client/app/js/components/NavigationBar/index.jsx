fce9940dd3e694e9bc43171aea4b1de2800887c5
Arthur <nesterenko19@gmail.com>
2017-04-29 17:48:15 +0300

    added private links to navbar

--- client/app/js/components/NavigationBar/index.jsx
+++ client/app/js/components/NavigationBar/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
+import { Session } from 'utils/Session';
+import { NavLink, Link } from 'react-router-dom';
 
 /**
  * TODO:
  */
-export default(props) => (
+export default( props ) => (
     <div>
         <ul>
             <NavLink to="/" activeClassName="active">Home</NavLink>
@@ -15,8 +16,27 @@ export default(props) => (
             <br/>
             <Link to="/signin">Login</Link>
             <br/>
-
             <Link to="/register">Register</Link>
         </ul>
+        <br/>
+        {Session.getToken() && <PrivateLinks />}
     </div>
+);
+
+/**
+ *  Render private links
+ * @constructor
+ */
+const PrivateLinks = () => (
+    <ul>
+        <li>
+            <NavLink to="/projects" activeClassName="active">Projects</NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard" activeClassName="active">Dashborad</NavLink>
+        </li>
+        <li>
+            <NavLink to="/userprofile" activeClassName="active">Userprofile</NavLink>
+        </li>
+    </ul>
 );
