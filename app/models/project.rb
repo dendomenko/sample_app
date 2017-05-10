ae79cf8e85e7074ed4605d070a0fd02a0b0273cf
dendomenko <dendomenko@gmail.com>
2017-05-10 15:48:15 +0300

    Add validations for project and return error array if not valid data

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
   has_many :users, through: :roles
 
   has_many :tasks
+
+  validates :name, length: {minimum: 3}
+  validates :task_name, length: {minimum: 2, maximum: 6}
+
 end
